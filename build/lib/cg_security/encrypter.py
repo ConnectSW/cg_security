6eb9807183854cb96f0a7c53501b968569578c7a
Roberto Mizuuti <rmizuuti@ronin>
2020-08-24 16:21:34 -0400

    Update encrypter.py, hasher.py, and 2 more files...

--- build/lib/cg_security/encrypter.py
+++ build/lib/cg_security/encrypter.py
@@ -6,10 +6,24 @@ from .security_exception import SecurityException
 class Encrypter:
 
     def __init__(self, key, salt=None):
+        """
+        Inicia o objeto com chave e salt
+        :param key: Chave de criptografia
+        :type key: str
+        :param salt: Salt criptográfico
+        :type salt: str
+        """
         self.key = key.encode('utf-8')
         self.salt = salt.encode('utf-8')
 
-    def encrypt(self, plaintext):
+    def encrypt(self, plaintext) -> str:
+        """
+        Criptografa uma string
+        :param plaintext: Texto a ser criptografado
+        :type plaintext: str
+        :return: Cifra do texto
+        :rtype: str
+        """
         if plaintext == "":
             raise SecurityException("Text to be encyrpted is empty")
         try:
@@ -19,7 +33,14 @@ class Encrypter:
         except Exception as e:
             raise SecurityException(f"Could not encrypt. {str(e)}")
 
-    def decrypt(self, encrypted):
+    def decrypt(self, encrypted) -> str:
+        """
+        Descriptografa uma cifra
+        :param encrypted: Cifra
+        :type encrypted: str
+        :return: Texto descriptografado
+        :rtype: str
+        """
         if encrypted == "":
             raise SecurityException("Text to be decrypted is empty")
         try:
